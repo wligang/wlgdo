addd9804307ae4a9b6546244c09912f10790ba31
wligang <wlgchun@163.com>
2018-05-29 15:35:20 +0800

    Update UserMapper.java

--- wlgdo-web-apartment/src/main/java/com/wlgdo/apartment/mapper/UserMapper.java
+++ wlgdo-web-apartment/src/main/java/com/wlgdo/apartment/mapper/UserMapper.java
@@ -4,12 +4,7 @@ import java.util.List;
 
 import com.wlgdo.common.model.User;
 
-/**
- * @desc提供对user对象的相关操作
- * @author: Ligang.Wang[wang_lg@suixingpay.com]
- * @date: 2017年11月10日 下午3:49:02
- * @Copyright ©2017 Suixingpay. All rights reserved.
- */
+
 public interface UserMapper {
 
 	// @Insert("insert into user(username,age,time) values
